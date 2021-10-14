95ac9f6dd21c12d7788b2a89bf2f1b3adb3e6619
kotomin.i <Antiphotons@gmail.com>
2021-10-14 17:17:27 +0300

    df merge

--- Training.py
+++ Training.py
@@ -1,8 +1,8 @@
 import pandas as pd
-voi = pd.DataFrame({
-    'pluses': [1, 2, 5, 8, 0],
-    'minuses': [-1, -2, -5, -8, 0]
-})
 
-voi_trnspz = pd.Series.transpose(voi['pluses'])
-print(voi_trnspz)
+voi_df = pd.read_csv('C:/Kotomin/Globalall/Methionine_dyn/02_TAC/VOI_TACs/SkrynnikovVA_all.csv', sep=',')
+voi_df = voi_df[voi_df.Volume != 'CT - CT 3.27 mm']
+voi_data = voi_df[voi_df.Stat == 'Макс'][['Patient Name', 'Volume', 'ROI', 'Stat', 'Value']].reset_index()
+del voi_data['index']
+
+print(voi_data)
--- VCAR_csv_process.py
+++ VCAR_csv_process.py
@@ -1,7 +1,29 @@
 import pandas as pd
 
-voi_df = pd.read_csv('C:/Kotomin/Globalall/Methionine_dyn/02_TAC/VOI_TACs/SkrynnikovVA_all.csv', sep=',')   # VOI info load
-voi_data = voi_df[['Patient Name', 'Volume', 'ROI', 'Stat', 'Value']]   # generating a new data frame with limited VOI info
+# VOI info load
+voi_df = pd.read_csv('C:/Kotomin/Globalall/Methionine_dyn/02_TAC/VOI_TACs/SkrynnikovVA_all.csv', sep=',')
+# delete CT information
+voi_df = voi_df[voi_df.Volume != 'CT - CT 3.27 mm']
+# generating a new data frame with limited VOI info
+voi_data = voi_df[voi_df.Stat == 'Макс'][['Patient Name', 'Volume', 'ROI', 'Stat', 'Value']].reset_index()
+del voi_data['index']
+
 # print(voi_df.iloc[1])
 # print(curves.loc[[1], ['Stat', 'Value']])
-print(voi_data)
+
+voi_max = voi_df[voi_df.Stat == 'Макс'][['Value']].reset_index()
+del voi_max['index']
+voi_mean = voi_df[voi_df.Stat == 'Средн.'][['Value']].reset_index()
+del voi_mean['index']
+voi_peak = voi_df[voi_df.Stat == 'Пик'][['Value']].reset_index()
+del voi_peak['index']
+
+example = pd.DataFrame({
+    'Series': voi_data.Volume,
+    'VOI': voi_data.ROI,
+    'Maximum': voi_max.Value,
+    'Mean': voi_mean.Value,
+    'Peak': voi_peak.Value
+})
+
+print(example)

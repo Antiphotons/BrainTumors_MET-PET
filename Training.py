643c7cf867aa0484764f2aa6fb59dd8f2c59b70c
kotomin.i <Antiphotons@gmail.com>
2021-10-15 19:57:48 +0300

    df merge

--- Training.py
+++ Training.py
@@ -1,8 +1,7 @@
+# print(voi_df.iloc[1]) example of indexation
+
 import pandas as pd
 
-voi_df = pd.read_csv('C:/Kotomin/Globalall/Methionine_dyn/02_TAC/VOI_TACs/SkrynnikovVA_all.csv', sep=',')
-voi_df = voi_df[voi_df.Volume != 'CT - CT 3.27 mm']
-voi_data = voi_df[voi_df.Stat == 'Макс'][['Patient Name', 'Volume', 'ROI', 'Stat', 'Value']].reset_index()
-del voi_data['index']
-
-print(voi_data)
+voi = pd.Series([1, 30, 4, 10])
+v = 'voi.csv'
+voi.to_csv(v, sep='\t')
--- VCAR_csv_process.py
+++ VCAR_csv_process.py
@@ -2,28 +2,43 @@ import pandas as pd
 
 # VOI info load
 voi_df = pd.read_csv('C:/Kotomin/Globalall/Methionine_dyn/02_TAC/VOI_TACs/SkrynnikovVA_all.csv', sep=',')
-# delete CT information
+# delete CT information and last strings
 voi_df = voi_df[voi_df.Volume != 'CT - CT 3.27 mm']
-# generating a new data frame with limited VOI info
-voi_data = voi_df[voi_df.Stat == 'Макс'][['Patient Name', 'Volume', 'ROI', 'Stat', 'Value']].reset_index()
-del voi_data['index']
+voi_df = voi_df[voi_df.ROI != 'PERCIST 1.0']
 
-# print(voi_df.iloc[1])
-# print(curves.loc[[1], ['Stat', 'Value']])
 
-voi_max = voi_df[voi_df.Stat == 'Макс'][['Value']].reset_index()
-del voi_max['index']
-voi_mean = voi_df[voi_df.Stat == 'Средн.'][['Value']].reset_index()
-del voi_mean['index']
-voi_peak = voi_df[voi_df.Stat == 'Пик'][['Value']].reset_index()
-del voi_peak['index']
+# function of VOI choosing
+def voi_choose(dataframe, voi):
+    df = dataframe[dataframe.ROI == voi]
+    return df
 
-example = pd.DataFrame({
-    'Series': voi_data.Volume,
-    'VOI': voi_data.ROI,
-    'Maximum': voi_max.Value,
-    'Mean': voi_mean.Value,
-    'Peak': voi_peak.Value
-})
 
-print(example)
+# generating the list of VOI
+voi_list = pd.Series(voi_df['ROI']).unique()
+
+# Particular VOI dataframes generation
+for i in range(len(voi_list)):
+    voi_df_nw = voi_choose(voi_df, voi_list[i])
+
+    # generating a new data frame with limited VOI info
+    voi_supp_info = voi_df_nw[voi_df_nw.Stat == 'Макс'][['Patient Name', 'Volume', 'ROI']].reset_index()
+    del voi_supp_info['index']
+
+    voi_max = voi_df_nw[voi_df_nw.Stat == 'Макс'][['Value']].reset_index()
+    del voi_max['index']
+    voi_mean = voi_df_nw[voi_df_nw.Stat == 'Средн.'][['Value']].reset_index()
+    del voi_mean['index']
+    voi_peak = voi_df_nw[voi_df_nw.Stat == 'Пик'][['Value']].reset_index()
+    del voi_peak['index']
+
+    voi_df_nw = pd.DataFrame({
+        'Series': voi_supp_info.Volume,
+        'VOI': voi_supp_info.ROI,
+        'Maximum': voi_max.Value,
+        'Mean': voi_mean.Value,
+        'Peak': voi_peak.Value
+    })
+
+    voi_name = voi_list[i]
+    print(voi_name)
+    #voi_df_nw.to_csv(voi_name, sep='\t')

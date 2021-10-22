def2a4471c0891de829d9a1220e0fd0ed11f79a8
kotomin.i <Antiphotons@gmail.com>
2021-10-22 17:52:06 +0300

    curve processing program initiation: function for transform voi dataframe to particular curves

---
+++ Curve_process.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import math as m
+
+
+# function for transform VOI file to curve dataframe
+def curve_loader(folder_path, file_name, measure_type):
+    path = folder_path
+    file = file_name  # input from user before function starts
+    voi_dataframe = pd.read_csv(path + file, sep='\t')  # previous created VOI .csv
+    # only dynamic data preserved
+    voi_dataframe = voi_dataframe[voi_dataframe.Series != 'PET - 10-30'].reset_index()
+    if len(voi_dataframe.VOI) < 26:
+        times = pd.Series([0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 360, 420,
+                           480, 540, 600, 780, 960, 1140, 1320, 1500, 1680, 1860, 2040, 2220])
+    elif 25 < len(voi_dataframe.VOI) < 71:
+        times = pd.Series([0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180, 195, 210,
+                           225, 240, 255, 270, 285, 300, 315, 330, 345, 360, 375, 390, 405, 420,
+                           435, 450, 465, 480, 495, 510, 525, 540, 555, 570, 585, 600, 630, 660,
+                           690, 720, 750, 780, 810, 840, 870, 900, 930, 960, 990, 1020, 1050, 1080,
+                           1110, 1140, 1170, 1200, 1320, 1440, 1560, 1680, 1800, 1920, 2040, 2160, 2280])
+    else:
+        print('error: check dynamic curve length')
+    curve_dataframe = voi_dataframe['']
--- Training.py
+++ Training.py
@@ -1,63 +1,8 @@
 import pandas as pd
 
-
-# function for load information from csv with VOI info
-def voi_loader(folder_path, file_number):
-    path = folder_path
-    file = "{0:0=3d}".format(file_number) + ".csv"
-    voi_dataframe = pd.read_csv(path + file, sep=',')
-    return voi_dataframe
-
-
-# function for VOI choosing
-def voi_choose(dataframe, voi):
-    df = dataframe[dataframe.ROI == voi]
-    return df
-
-
-# function for sorting VOI info into separate csv files
-def voi_separation(patient_number, voi_df):
-    # delete CT information and last strings
-    voi_df = voi_df[voi_df.Volume != 'CT - CT 3.27 mm']
-    voi_df = voi_df[voi_df.ROI != 'PERCIST 1.0']
-
-    # generating the list of VOI
-    voi_list = pd.Series(voi_df['ROI']).unique()
-
-    # Particular VOI dataframes generation
-    for j in range(len(voi_list)):
-        voi_df_nw = voi_choose(voi_df, voi_list[j])
-
-        # generating a new data frame with limited VOI info
-        voi_supp_info = voi_df_nw[voi_df_nw.Stat == 'Макс'][['Patient Name', 'Volume', 'ROI', 'Unit']].reset_index()
-        del voi_supp_info['index']
-
-        voi_max = voi_df_nw[voi_df_nw.Stat == 'Макс'][['Value']].reset_index()
-        del voi_max['index']
-        voi_mean = voi_df_nw[voi_df_nw.Stat == 'Средн.'][['Value']].reset_index()
-        del voi_mean['index']
-        voi_peak = voi_df_nw[voi_df_nw.Stat == 'Пик'][['Value']].reset_index()
-        del voi_peak['index']
-
-        voi_df_nw = pd.DataFrame({
-            'Series': voi_supp_info.Volume,
-            'VOI': voi_supp_info.ROI,
-            'Units': voi_supp_info.Unit,
-            'Maximum': voi_max.Value,
-            'Mean': voi_mean.Value,
-            'Peak': voi_peak.Value
-        })
-
-        voi_name = patient_number + '_' + voi_list[j] + '.csv'
-        print(voi_name)
-        voi_df_nw.to_csv(voi_name, sep='\t')
-
-
-# VOI info load in dataframe
-path_to_vois_folder = 'C:/Kotomin/Globalall/Methionine_dyn/02_TAC/VOI_TACs/'
-
-for i in range(3):
-    file_num = i + 1
-    patient_num = "{0:0=3d}".format(file_num)
-    voi_df_unsort = voi_loader(path_to_vois_folder, file_num)
-    voi_separation(patient_num, voi_df_unsort)
+times = pd.Series([0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180, 195, 210,
+                           225, 240, 255, 270, 285, 300, 315, 330, 345, 360, 375, 390, 405, 420,
+                           435, 450, 465, 480, 495, 510, 525, 540, 555, 570, 585, 600, 630, 660,
+                           690, 720, 750, 780, 810, 840, 870, 900, 930, 960, 990, 1020, 1050, 1080,
+                           1110, 1140, 1170, 1200, 1320, 1440, 1560, 1680, 1800, 1920, 2040, 2160, 2280])
+print(times)

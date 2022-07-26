2d6bee40a6d59fe487632b7dab0f84cef8aab53a
Antiphotons <Antiphotons@gmail.com>
2022-07-26 22:30:27 +0300

    MET_Intervals_statistics upd: residuals

--- MET_Intervals_statistics.py
+++ MET_Intervals_statistics.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# Function for computation medians and quartiles of indexes
+# Function for computation of medians and quartiles of indexes
 def column_median(dataframe, column):
     median = np.percentile(dataframe[column], 50)
     low_quartile = np.percentile(dataframe[column], 25)
@@ -12,11 +12,57 @@ def column_median(dataframe, column):
     return str(median) + ' (' + str(low_quartile) + '–' + str(high_quartile) + ')'
 
 
+# Function for computation of residuals
+def residuals(dataframe, parameter):
+    columnst = parameter + '-st'
+    column1 = parameter + '-1'
+    column2 = parameter + '-2'
+    column3 = parameter + '-3'
+    res_st1 = dataframe[columnst] - dataframe[column1]
+    res_st2 = dataframe[columnst] - dataframe[column2]
+    res_st3 = dataframe[columnst] - dataframe[column3]
+    res_21 = dataframe[column2] - dataframe[column1]
+    res_31 = dataframe[column3] - dataframe[column1]
+    res_32 = dataframe[column3] - dataframe[column2]
+    res_df = pd.DataFrame({
+        parameter + '_st-1': res_st1,
+        parameter + '_st-2': res_st2,
+        parameter + '_st-3': res_st3,
+        parameter + '_2-1': res_21,
+        parameter + '_3-1': res_31,
+        parameter + '_3-2': res_32
+    })
+    return res_df
+
+
+# Function for computation of percent residuals
+def rel_residuals(dataframe, parameter):
+    columnst = parameter + '-st'
+    column1 = parameter + '-1'
+    column2 = parameter + '-2'
+    column3 = parameter + '-3'
+    rel_res_st1 = (dataframe[columnst] - dataframe[column1]) / dataframe[columnst]
+    rel_res_st2 = dataframe[columnst] - dataframe[column2] / dataframe[columnst]
+    rel_res_st3 = dataframe[columnst] - dataframe[column3] / dataframe[columnst]
+    rel_res_21 = dataframe[column2] - dataframe[column1] / dataframe[column1]
+    rel_res_31 = dataframe[column3] - dataframe[column1] / dataframe[column1]
+    rel_res_32 = dataframe[column3] - dataframe[column2] / dataframe[column1]
+    rel_res_df = pd.DataFrame({
+        parameter + '_st-1': rel_res_st1,
+        parameter + '_st-2': rel_res_st2,
+        parameter + '_st-3': rel_res_st3,
+        parameter + '_2-1': rel_res_21,
+        parameter + '_3-1': rel_res_31,
+        parameter + '_3-2': rel_res_32
+    })
+    return rel_res_df
+
+
 # path to csv-file with data
 folder = 'C:/Kotomin/Globalall/Methionine_dyn/01_Intervals/'
 file = 'Intervals.csv'
 
-# empty rows deletion
+# df load & empty rows deletion
 Int_dataframe = pd.read_csv(folder + file, sep='\t')
 Int_dataframe = Int_dataframe[Int_dataframe.Case != ''].reset_index()
 del Int_dataframe['Index']
@@ -38,3 +84,8 @@ for prmtr in parameters:
         p = prmtr + '-' + intrv
         med_quart = column_median(Int_dataframe, p)
         median_df[intrv].loc[prmtr] = med_quart
+
+    res_dataframe = residuals(Int_dataframe, prmtr)
+    res_dataframe.to_csv(prmtr + '_residuals.csv', sep='\t')
+    rel_res_dataframe = residuals(Int_dataframe, prmtr)
+    rel_res_dataframe.to_csv(prmtr + '_relative_residuals.csv', sep='\t')

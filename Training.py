4ea9af5c7ef3f8daf1a501298cbe41b15232e7b0
kotomin.i <Antiphotons@gmail.com>
2022-11-22 17:39:03 +0300

    introduce Brown-Forsythe Test to intervals statistics

--- Training.py
+++ Training.py
@@ -2,7 +2,17 @@ import os.path
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import levene
 
-parameters = [[0.00001, 0.00001, 0.03278, 0.00003, 0.45510, 0.18604, 0.21214, 0.77499], 'huets']
-parameters = [round(parameters[0][i], 5) for i in range(len(parameters[0]))]
-print(parameters)
+df = pd.DataFrame({
+    '1': [1, 5, 9, 13, 17],
+    '2': [1, 3, 5, 7, 9]
+})
+
+var1 = np.var(df['1'])
+var2 = np.var(df['2'])
+lev = levene(df['1'], df['2'], center='median')
+
+print(df)
+print(var1, var2)
+print(lev)

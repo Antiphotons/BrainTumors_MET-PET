0514a6fc73c6c1e86a2befeb2b1b85edb5423081
kotomin.i <Antiphotons@gmail.com>
2022-11-28 20:48:00 +0300

    Brown-Forsythe update – finished variances df

--- MET_Intervals_progect/brown_forsythe.py
+++ MET_Intervals_progect/brown_forsythe.py
@@ -3,8 +3,8 @@ import numpy as np
 from scipy.stats import levene
 
 
-def brown_forsythe(dataframe, column):
-    stats, bf = levene(dataframe[column], dataframe[column], center='median')
+def brown_forsythe(array1, array2):
+    stats, bf = levene(array1, array2, center='median')
     return bf
 
 
@@ -12,12 +12,21 @@ folder = 'C:/Kotomin/Globalall/Methionine_dyn/01_Intervals/csv/'
 file = 'residuals.csv'
 
 df = pd.read_csv(folder + file, sep='\t')
-params = [df.columns[i].split('_')[0] for i in range(1, df.shape[1], 6)]
-resids = [df.columns[i].split('_')[1] for i in range(1, 7)]
+params = [df.columns[i].split('_')[0] for i in range(1, df.shape[1], 6)]  # derive parameters names
+resids = [df.columns[i].split('_')[1] for i in range(1, 7)][0:3]  # derive residuals names and filter first three
 
-var_df = pd.DataFrame(data=None, index=params, columns=resids)
+var_df = pd.DataFrame(data=None, index=params, columns=resids)  # empty dataframe for variances
+bf_df = pd.DataFrame(data=None, index=df.columns[1:], columns=df.columns[1:])  # empty df for brown-forsythe p-values
 
-for i in range(1, df.shape[1]):
-    col = df.columns[i]
-    variance = round(np.var(df[col]), 3)
-    #print(variance)
+for r in resids:
+    prev_col = df[params[0] + '_' + r]
+    for p in params:
+        col = df[p + '_' + r]
+        var_df[r][p] = (round(np.var(col), 3))
+        stats, bf = levene(col, prev_col, center='median')
+        #print(round(bf, 4))
+        prev_col = col
+
+
+print(var_df)
+print(bf_df)
--- Training.py
+++ Training.py
@@ -4,15 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import levene
 
-df = pd.DataFrame({
-    '1': [1, 5, 9, 13, 17],
-    '2': [1, 3, 5, 7, 9]
-})
+params = ['a', 'b', 'c']
+resids = ['1-1', '2-2', '3-3']
 
-var1 = np.var(df['1'])
-var2 = np.var(df['2'])
-lev = levene(df['1'], df['2'], center='median')
-
-print(df)
-print(var1, var2)
-print(lev)
+var_df = pd.DataFrame(data=None, index=params, columns=resids)
+var_df['2-2']['c'] = 'dfjng'
+print(var_df)

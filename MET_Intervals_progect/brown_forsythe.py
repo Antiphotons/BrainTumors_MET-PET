97f339f3ef6122dbd9e306d4973ab2a8c5ef3b64
kotomin.i <Antiphotons@gmail.com>
2022-12-12 20:09:03 +0300

    Brown-Forsythe update – median absolute deviation from medians computation added.

--- MET_Intervals_progect/brown_forsythe.py
+++ MET_Intervals_progect/brown_forsythe.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import levene
+from scipy.stats import median_abs_deviation
 
 
-def variances(path):
+# function for computation of residual variances
+def variances(path, dig):
     df = pd.read_csv(path, sep='\t')
     params = [df.columns[i].split('_')[0] for i in range(1, df.shape[1], 6)]  # derive parameters names
     resids = [df.columns[i].split('_')[1] for i in range(1, 7)][0:3]  # derive residuals names and filter first three
@@ -12,11 +14,25 @@ def variances(path):
     for r in resids:
         for p in params:
             col = df[p + '_' + r]
-            var_df[r][p] = (round(np.var(col), 3))
+            var_df[r][p] = (round(np.var(col), dig))
     return var_df
 
 
-def brown_forsythe(path):
+# function for computation of residual median absolute deviations
+def mad(path, dig):
+    df = pd.read_csv(path, sep='\t')
+    params = [df.columns[i].split('_')[0] for i in range(1, df.shape[1], 6)]  # derive parameters names
+    resids = [df.columns[i].split('_')[1] for i in range(1, 7)][0:3]  # derive residuals names and filter first three
+    mad_df = pd.DataFrame(data=None, index=params, columns=resids)  # empty dataframe for variances
+
+    for r in resids:
+        for p in params:
+            col = df[p + '_' + r]
+            mad_df[r][p] = (round(median_abs_deviation(col), dig))
+    return mad_df
+
+
+def brown_forsythe(path, dig):
     df = pd.read_csv(path, sep='\t')
     new_columns = [df.columns[i] for i in range(1, len(df.columns)) if i % 6 in [1, 2, 3]]  # derive st-i columns names
     bf_df = pd.DataFrame(data=None, index=new_columns, columns=new_columns)  # empty df for brown-forsythe p-values
@@ -26,7 +42,7 @@ def brown_forsythe(path):
         for c2 in bf_df.columns:
             col2 = df[c2]
             stats, bf = levene(col1, col2, center='median')
-            bf_df[c1][c2] = round(bf, 4)
+            bf_df[c1][c2] = round(bf, dig)
     return bf_df
 
 
@@ -34,10 +50,10 @@ folder = 'C:/Kotomin/Globalall/Methionine_dyn/01_Intervals/csv/'
 file1 = 'residuals.csv'
 file2 = 'relative_residuals.csv'
 
-res_var_df, res_bf_df = variances(folder + file1), brown_forsythe(folder + file1)
-rel_res_var_df, rel_res_bf_df = variances(folder + file2), brown_forsythe(folder + file2)
+res_mad_df, res_bf_df = mad(folder + file1, 3), brown_forsythe(folder + file1, 5)
+rel_res_mad_df, rel_res_bf_df = mad(folder + file2, 0), brown_forsythe(folder + file2, 5)
 
-res_var_df.to_csv('residuals_variances.csv', sep='\t')
-rel_res_var_df.to_csv('rel_residuals_variances.csv', sep='\t')
+res_mad_df.to_csv('residuals_mad.csv', sep='\t')
+rel_res_mad_df.to_csv('rel_residuals_mad.csv', sep='\t')
 res_bf_df.to_csv('residuals_bf.csv', sep='\t')
 rel_res_bf_df.to_csv('rel_residuals_bf.csv', sep='\t')
